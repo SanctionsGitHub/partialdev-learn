df98b1c3c2fc97f2941a06daa406ecddaecd439a
Tristian Kelly <tristian.kelly560@gmail.com>
2018-02-07 07:31:08 -0600

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,10 +1,8 @@
 const themes = require('./themes')
-const tutorials = require('./tutorials')
 module.exports = {
   site: {
     title: 'Luxaura.js',
     description: 'A simple static site generator',
-    themes,
-    tutorials
+    themes
   }
 }
