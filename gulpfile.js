de75baf4c861ea6585c91e1b62ffb55b164ac2a8
Tristian Kelly <tristian.kelly560@gmail.com>
2018-03-13 12:47:57 -0500

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var less = require('gulp-less');
+var path = require('path');
+ 
+gulp.task('default', function () {
+     return gulp.src('./styles.less')
+    .pipe(less())
+    .pipe(gulp.dest('./site/css/main.css'));
+});
