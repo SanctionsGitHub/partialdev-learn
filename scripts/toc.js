38e1646d038c53dd657610e1590b9b25e297a44b
Tristian Kelly <tristian.kelly560@gmail.com>
2018-03-13 11:16:40 -0500

    Update toc.js

--- scripts/toc.js
+++ scripts/toc.js
@@ -93,7 +93,7 @@ module.exports = function(md, options) {
           // Finishing the sub headings
           buffer += "</li>";
           headings.push(buffer);
-          return [i, "<" + options.listType + "uk-scrollspy-nav=\"closest: li; scroll: true; offset: 100\">" + headings.join("") + "</" + options.listType + ">"];
+          return [i, "<" + options.listType + 'uk-scrollspy-nav="closest: li; scroll: true; offset: 100">' + headings.join("") + "</" + options.listType + ">"];
         }
         if (level == currentLevel) {
           // Finishing the sub headings
@@ -108,7 +108,7 @@ module.exports = function(md, options) {
     }
     buffer += buffer === "" ? "" : "</li>";
     headings.push(buffer);
-    return [i, "<" + options.listType + ">" + headings.join("") + "</" + options.listType + ">"];
+    return [i, "<" + options.listType + 'uk-scrollspy-nav="closest: li; scroll: true; offset: 100">' + headings.join("") + "</" + options.listType + ">"];
   }
 
   // Catch all the tokens for iteration later
