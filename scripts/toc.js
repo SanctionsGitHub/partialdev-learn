bb53695dd9a59a27bc3461d2a1d98083e469317c
Tristian Kelly <tristian.kelly560@gmail.com>
2018-03-14 07:59:16 -0500

    Update toc.js

--- scripts/toc.js
+++ scripts/toc.js
@@ -2,7 +2,7 @@
 var string = require("string");
 var assign = require("lodash.assign");
 var defaults = {
-  includeLevel: [ 2 ],
+  includeLevel: [ 2, 3 ],
   containerClass: "table-of-contents",
   slugify: function(str) {
     return string(str).slugify().toString();
