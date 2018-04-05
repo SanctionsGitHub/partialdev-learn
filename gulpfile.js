8e4d319afe70c652e76d44895c581b5aea48518e
Joel Bond <joel.bond@partialdev.com>
2018-04-05 14:18:50 -0600

    Removed sitemap gulp code

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,3 @@ gulp.task('default', function () {
     .pipe(less())
     .pipe(gulp.dest('./css'));
 });
-
-var sitemap = require('gulp-sitemap');
-
-gulp.task('sitemap', function () {
-    gulp.src('site/**/*.html', {
-            read: false
-        })
-        .pipe(sitemap({
-            siteUrl: 'https://learn.partialdev.com'
-        }))
-        .pipe(gulp.dest('./site'));
-});
-gulp.start.apply(gulp, ['sitemap']);
