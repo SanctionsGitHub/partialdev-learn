8366063c1369e33c7cf16fe6005680cb7f25d787
SanctionsGitHub <35189209+SanctionsGitHub@users.noreply.github.com>
2018-04-05 22:24:26 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
+
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var sitemap = require('gulp-sitemap');
 
 gulp.task('default', function () {
      return gulp.src('./less')
     .pipe(less())
     .pipe(gulp.dest('./css'));
 });
+
+gulp.task('default', function () {
+    gulp.src('site/**/*.html', {
+            read: false
+        })
+        .pipe(sitemap({
+            siteUrl: 'https://learn.partialdev.com'
+        }))
+        .pipe(gulp.dest('./site'));
