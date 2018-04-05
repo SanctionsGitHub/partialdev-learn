ac7e2b38154f36cd2304157c151e1fccc2ffb7aa
Joel Bond <joel.bond@zaptech.ca>
2018-04-05 13:19:16 -0600

    adjusted the gulpfile, renamed main.less, and updated head.html

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-var less = require('gulp-less');
-var path = require('path');
- 
-gulp.task('default', function () {
-     return gulp.src('./less/main.less')
-    .pipe(less())
-    .pipe(gulp.dest('./site/css'));
-});
