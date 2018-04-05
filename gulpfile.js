cbae12ed22bed7ef53c5a303196babc4b0d9d931
SanctionsGitHub <35189209+SanctionsGitHub@users.noreply.github.com>
2018-04-05 21:34:31 +0200

    Learn v0.0.1 (#1)
    * Update main.less
    
    * Update head.html
    
    * Update foot.html
    
    * Delete Thumbs.db
    
    * Delete cone.png
    
    * Delete favicon.png
    
    * Delete luxaura.png
    
    * Create index.html
    
    * Add files via upload
    
    * Add files via upload
    
    * Update and rename navbar.html to header.html
    
    * Rename header.html to navbar.html
    
    * Update index.html
    
    * Delete uikit-icons.min.js
    
    * Delete uikit.min.js
    
    * Update smooth-scroll.min.js
    
    * Delete sidebar.html
    
    * Update navbar.html
    
    * Update site.config.js
    
    * removed sidebar
    
    * Delete index.html
    
    * removed sidebar
    
    * adjusted the gulpfile, renamed main.less, and updated head.html
    
    * added default code to gulpfile so it didn't get mad at me
    
    * updated gitignore
    
    * add gulp-sitemap to package.json
    
    * fixed a stupid mistake
    
    * Update head.html
    
    * fixed my stupid mistake
    
    * removed the .. as the css files are now in the static director
    
    * removed the /
    
    * Update head.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require("gulp");
-var less = require('gulp-less');
 var path = require('path');
- 
+var sitemap = require('gulp-sitemap');
+
 gulp.task('default', function () {
-     return gulp.src('./less/main.less')
-    .pipe(less())
-    .pipe(gulp.dest('./site/css'));
+    gulp.src('site/**/*.html', {
+            read: false
+        })
+        .pipe(sitemap({
+            siteUrl: 'https://learn.partialdev.com'
+        }))
+        .pipe(gulp.dest('./site'));
 });
