948541c9e4791aab876283873a03ea385f6cd47b
Joel Bond <joel.bond@zaptech.ca>
2018-04-05 13:21:16 -0600

    added default code to gulpfile so it didn't get mad at me

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var path = require('path');
+var sitemap = require('gulp-sitemap');
+
+gulp.task('default', function () {
+    gulp.src('site/**/*.html', {
+            read: false
+        })
+        .pipe(sitemap({
+            siteUrl: 'https://partialdev.com'
+        }))
+        .pipe(gulp.dest('./site'));
+});
