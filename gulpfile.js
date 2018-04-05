f319f25e0251af2be48a5a9035d136e2db0e5dd1
SanctionsGitHub <35189209+SanctionsGitHub@users.noreply.github.com>
2018-04-05 22:17:41 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 var path = require('path');
 
 gulp.task('default', function () {
-     return gulp.src('./less/')
+     return gulp.src('./less')
     .pipe(less())
     .pipe(gulp.dest('./css'));
 });
